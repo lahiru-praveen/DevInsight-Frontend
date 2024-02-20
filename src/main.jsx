15f169c34c28c2c1b98ab88346eefa230396c833
Lahiru <praveenwal.21@uom.lk>
2024-02-21 01:05:19 +0530

    Forget-Password-Start

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+//import './index.css'
+import {ChakraProvider} from "@chakra-ui/react";
+import ForgetPassword from "./components/forget-password-com1.jsx";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+      <ChakraProvider>
+          {/*<App/>*/}
+          <ForgetPassword/>
+      </ChakraProvider>
+
+  </React.StrictMode>
 )
---
+++ src/pages/forget-password.jsx
@@ -0,0 +1,9 @@
+function page() {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
+
+//import forgetPasswordCom1 from "../components/forget-password-com1.jsx";
