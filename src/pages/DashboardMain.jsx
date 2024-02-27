4c40384d985804d4870236705c44f9c578511421
Lahiru <praveenwal.21@uom.lk>
2024-02-27 23:14:35 +0530

    Main Dashboard working

--- src/pages/DashboardMain.jsx
+++ src/pages/DashboardMain.jsx
@@ -1,14 +1,10 @@
 import { useState } from 'react';
-import { Button, Tabs, TabList, TabPanels, Tab, TabPanel, Textarea, Text, Box , Select } from '@chakra-ui/react';
+import { Button, Tabs, TabList, TabPanels, Tab, TabPanel, Textarea, Text, Box, Select } from '@chakra-ui/react';
+import { AiOutlineFolderAdd,AiOutlineFileAdd } from "react-icons/ai";
 import PreviousSubmissionRight1 from "../components/dashboard/PreviousSubmissionRight1.jsx";
 
 export default function DashboardMain() {
     const [files, setFiles] = useState(null);
-
-    // const handleUpload = (event) => {
-    //     const fileList = event.target.files;
-    //     setFiles(fileList);
-    // };
 
     const handleDrop = (event) => {
         event.preventDefault();
@@ -21,49 +17,53 @@ export default function DashboardMain() {
     };
 
     return (
-        <div className="flex flex-col h-screen" onDrop={handleDrop} onDragOver={handleDragOver}>
-
-            <div className="h-[70px] bg-black w-full">
+        <div className="h-screen">
+            <div className="h-[80px] bg-black w-full">
                 <h1>Helllo</h1>
             </div>
 
-            <div className="flex flex-1">
-                <div className="w-[260px] bg-blue-600 p-[15px] hidden md:block">
-                    <PreviousSubmissionRight1 />
+            <div className="grid grid-cols-1 md:grid-cols-6 gap-0">
+                <div className="bg-blue-500 h-full">
+                    <PreviousSubmissionRight1/>
                 </div>
 
-                <div className="flex-1 p-[15px]">
-                    <Button isDisabled size='md' height='48px' width='200px' border='2px' borderColor='blue.500'
-                            textColor='blue.500' className="flex float-right">Submit</Button>
-                    <br /><br /><br />
-                    <div>
+                <div className="col-span-1 md:col-span-5 p-[15px]">
+                    <div className="flex justify-end mb-4">
+                        <Button isDisabled border='2px' size="md" borderColor='blue.500' textColor='blue.500'>
+                            Submit
+                        </Button>
+                    </div>
+
+                    <div className="p-[15px]">
                         <Tabs isFitted variant='enclosed'>
                             <TabList mb='1em'>
-                                <Tab _selected={{ color: 'white', bg: 'blue.500' }} fontSize="18px">Upload File</Tab>
-                                <Tab _selected={{ color: 'white', bg: 'blue.500' }} fontSize="18px">Upload Code</Tab>
+                                <Tab _selected={{color: 'white', bg: 'blue.500'}} fontSize="18px">Upload File</Tab>
+                                <Tab _selected={{color: 'white', bg: 'blue.500'}} fontSize="18px">Upload Code</Tab>
                             </TabList>
                             <TabPanels>
                                 <TabPanel>
-                                    <Textarea bgColor={'#EBEBEB'} color={'#646464'} height="120px" fontSize="18px" className="flex" placeholder='Enter Key words about your code' />
+                                    <Textarea bgColor={'#EBEBEB'} color={'#646464'} height="120px" fontSize="18px" placeholder='Enter Key words about your code'/>
                                     <Text fontSize='18px' className="font-bold">Upload the source file or Project folder </Text>
-                                        <div>
-                                            <Box onDrop={handleDrop} onDragOver={handleDragOver} mt={4} p={4} borderWidth="1px" borderRadius="md" bgColor={'#EBEBEB'} color={'#646464'} height="450px" className="flex">
-                                                {files && (
-                                                    <ul>
-                                                        {Array.from(files).map((file, index) => (
-                                                            <li key={index}>{file.name}</li>
-                                                        ))}
-                                                    </ul>
-                                                )}
-                                                {!files && (
-                                                    <p>Drag and drop files here or click to select files</p>
-                                                )}
-                                            </Box>
-                                        </div>
+                                    <div>
+                                        <Box onDrop={handleDrop} onDragOver={handleDragOver} mt={4} p={4} borderWidth="1px" borderRadius="md" bgColor={'#EBEBEB'} color={'#646464'} className="h-[450px]">
+                                            {files && (
+                                                <ul>
+                                                    {Array.from(files).map((file, index) => (
+                                                        <li key={index}>{file.name}</li>
+                                                    ))}
+                                                </ul>
+                                            )}
+                                            {!files && (
+                                                <p>Drag and drop files here or click to select files</p>
+                                            )}
+                                        </Box>
+                                        <AiOutlineFolderAdd />
+                                        <AiOutlineFileAdd />
+                                    </div>
                                 </TabPanel>
                                 <TabPanel>
                                     <div>
-                                        <Textarea bgColor={'#EBEBEB'} color={'#646464'} height="120px" fontSize="18px" className="flex" placeholder='Enter Key words about your code' /><br/>
+                                        <Textarea bgColor={'#EBEBEB'} color={'#646464'} height="120px" fontSize="18px" placeholder='Enter Key words about your code'/><br/>
                                         <Text fontSize='18px' className="font-bold">Upload the source file or Project folder </Text><br/>
                                         <Select placeholder='Select Language' className="w-[320px]">
                                             <option value="python">Python</option>
@@ -76,9 +76,10 @@ export default function DashboardMain() {
                                             <option value="swift">Swift</option>
                                             <option value="go">Go</option>
                                             <option value="typescript">TypeScript</option>
-                                            <option value="typescript">Other</option>
+                                            <option value="other">Other</option>
                                         </Select><br/>
-                                        <Textarea bgColor={'#EBEBEB'} color={'#646464'} height="400px" fontSize="18px" className="flex" placeholder='Past code to here' />
+                                        <Textarea bgColor={'#EBEBEB'} color={'#646464'} height="400px" fontSize="18px" placeholder='Paste code here'/>
+
                                     </div>
                                 </TabPanel>
                             </TabPanels>
@@ -86,7 +87,7 @@ export default function DashboardMain() {
                     </div>
                 </div>
             </div>
-
         </div>
     );
 }
+
